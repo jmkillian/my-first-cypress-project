7e747d765000e216f2c8c9910783954f2be65a00
jmkillian <jkchem9120@gmail.com>
2024-03-25 13:02:28 -0700

    first cypress tests

---
+++ cypress/e2e/addContact.spec.cy.js
@@ -0,0 +1,5 @@
+describe('Add Contact Tests', () => {  // arrow function is shorthand to get one function to call another
+  it('Can add a new contact', () => {
+    cy.visit('https://thinking-tester-contact-list.herokuapp.com')
+  })
+})
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,5 @@
+describe('template spec', () => {  // arrow function is shorthand to get one function to call another
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
